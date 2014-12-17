b97b5a10dbcc459f2332c5126b5666decb81bd99
Zheng <fzhpanda@gmail.com>
2014-12-16 22:32:04 -0600

    attached the fastest method

--- FirstMissingPositive.cpp
+++ FirstMissingPositive.cpp
@@ -17,3 +17,25 @@ public:
 (1) empty case, return 1;
 (2) missing 1: 2 3 4 -1 
 (3) missing 2: 3 1 4 -1 
+// note here is a fastest idea:  very tricky to modify the original array to make it so simple
+also O(1) space complexity
+
+int firstMissingPositive(int A[], int n) {
+if(n==0)
+    return 1;
+for (int i(0); i != n;)
+{
+    if (A[i] != i + 1 && A[i]>0 && A[i] <= n&& A[A[i]-1] != A[i])
+    {
+        swap(A[i], A[A[i] - 1]);
+    }
+    else ++i;
+}
+for (int i(0); i != n; ++i)
+{
+    if (A[i] != i + 1)
+        return i + 1;
+}
+return n+1;
+
+}
